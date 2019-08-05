608e9f7c3013d6ef5985dc3b4f8b29c87d301ed3
junkobe <727979074@qq.com>
2019-08-05 23:32:00 +0800

    服务发现方式获取配置信息

--- user-db/src/main/java/Application.java
+++ user-db/src/main/java/Application.java
@@ -1,7 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author: JUN
  * @Date: 2019/8/1 15:46
  */
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
